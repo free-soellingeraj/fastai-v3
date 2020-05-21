dc292052fddc965bda681bfb088b2a34fee5ea9b
Aaron Soellinger <asoellin@gmail.com>
2020-05-21 09:37:20 +0000

    added details from prod-emotion-image model see: .../course-v3/nbs/dl1/emotions-Productionizer.ipynb

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=10ex3tUtl73PQk8fMNO8jyj-3FeukBLtZ'
+export_file_name = 'prod-emotion-image.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Neutral', 'Sadness', 'Surprise']
 path = Path(__file__).parent
 
 app = Starlette()
